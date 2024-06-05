1a6dec0373934b0cd15dd875080861487eb09e1b
RebecaFernn <rebeca.santos@sptech.school>
2024-06-05 11:11:17 -0700

    Web data viz integrado

---
+++ web-data-viz/public/js/alerta.js
@@ -0,0 +1,11 @@
+var alertas = [];
+
+
+function exibirCards() {
+    alerta.innerHTML = '';
+
+    for (var i = 0; i < alertas.length; i++) {
+        var mensagem = alertas[i];
+        alerta.innerHTML += transformarEmDiv(mensagem);
+    }
+}
